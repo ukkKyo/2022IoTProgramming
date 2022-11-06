311ef74fec49f523b6153b4aa3183eab87f95a3e
ukkKyo <68654501+ukkKyo@users.noreply.github.com>
2022-11-06 17:31:39 +0900

    Update chat_server.c

--- Lab3/chat_server.c
+++ Lab3/chat_server.c
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
 
   if(bind(serv_sock, (struct sockaddr*) &serv_adr, sizeof(serv_adr))==-1)
     error_handling("bind() error");
-  if(listen(Serv_sock, 5)==-1)
+  if(listen(serv_sock, 5)==-1)
     error_handling("listen() error");
 
   while(1)
