99c9d865a6651be66a5e03d4ad72855efba3ef5f
ukkKyo <68654501+ukkKyo@users.noreply.github.com>
2022-11-17 22:16:19 +0900

    Create 4-3-2.c

---
+++ lab4/4-3-2.c
@@ -0,0 +1,29 @@
+// PWM LED 밝기 조절
+
+#include <stdio.h>
+#include <pigpio.h>
+
+int main()
+{
+  int led1 = 12;
+  int step = 10000;
+  int delay = 10000;
+  int freq = 10000;
+  int duty = 0;
+  
+  gpioInitialise();
+  
+  while(1) {
+    duty += step; // duty cycle 증가 또는 감소, step의 +,- 값에 따라 달라짐.
+    if(duty >= 1000000 || duty <= 0) { //duty cycle이 100%나 0%면 역방향 전환
+      step = -step;
+    }
+    gpioHardwarePWM(led, freq, duty);
+    
+    gpioDelay(delay); // delay만큼 정지
+  }
+  
+  gpioTerminate();
+  
+  return 0;
+}
